ef647de00179c47ec2878da0633c83a738481744
InternetED <salahayo3192@gmail.com>
2022-02-17 23:44:17 +0800

    build: Initialize this module.

---
+++ androidcommon/src/main/java/com/interneted/androidcommon/BasicGlideAppModule.kt
@@ -0,0 +1,20 @@
+package com.interneted.androidcommon
+
+import android.content.Context
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+/**
+ * Creator: ED
+ * Date: 2022/2/17 11:22 下午
+ * Mail: salahayo3192@gmail.com
+ *
+ * **/
+@GlideModule
+open class BasicGlideAppModule : AppGlideModule() {
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        super.applyOptions(context, builder)
+    }
+}
+
